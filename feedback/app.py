c36c1386bf7380e44170ba12f8eaba2eb1cace40
Milica Mladjenovic <milicamladjenovic95@gmail.com>
2022-09-15 09:25:50 +0200

    Updated feedback service to read data from other APIs

--- feedback/app.py
+++ feedback/app.py
@@ -12,16 +12,21 @@ db=connection.get_database()
 app = Flask(__name__)
 
 def get_ner_probabilites(text):
-    r = requests.post(url='http://obner/ner',json={'text':text})
-    return r['labels']
+    r = requests.post(url='http://obner:5000/ner',json={'text':text})
+    print(r)
+    return r
 
 def get_model_predictions(text):
     r = requests.post(url='http://model_serve/invocations',json={'columns':["text"],'data':text})
     return r
 
 def get_kb_labels(text):
-    r = requests.post(url='http://knowledge_base/label',json={'text':text})
+    r = requests.post(url='http://knowledge-base:5000/label',json={'text':text})
+    print(r)
     return r
+
+
+
 
 
 @app.route("/feedback", methods=["POST"])
@@ -32,21 +37,25 @@ def process():
     url =  article["url"]
     labels =  article["labels"]
 
-    probabilites = get_ner_probabilites(text)
-    predictions = get_model_predictions(text)
-    kb_labels = get_kb_labels(text)
+    user_id = request.headers.get('X-User')
+
+    probabilites = get_ner_probabilites(text).json().get('status')
+    # predictions = get_model_predictions(text)
+    kb_labels = get_kb_labels(text).json().get('status')
 
     penalty = 0
 
     for label in labels:
-        if probabilites[label] < 0.5:
+        if probabilites.get(label,0) < 0.5:
             print('Less likely to be the correct label if there are not enough entities connected to the label')
             penalty += 1
-        if label in predictions:
-            continue
+        # if label in predictions:
+        #     continue
         if label not in kb_labels:
             print("Knowledge base doesn't recognize any tokens related to this label. Either the knowledge base is poor or this label is wrong")
             penalty += 0.5
+
+    db.feedback.insert_one({'user_id':user_id,'penalty':penalty,'url':url,'text':text,'labels':labels})
 
     
         
